9c6685e18e799aa84206ca66acf403e5d40ffdb8
Oleg Nikitin <5qub6pfp@duck.com>
2024-04-06 20:39:18 +0300

    modules(os): update podman

--- modules/os/programs/podman/default.nix
+++ modules/os/programs/podman/default.nix
@@ -1,18 +1,36 @@
-{ lib, config, ... }: {
+{ pkgs, lib, config, ... }: {
   options = {
     module.program.podman.enable =
       lib.mkEnableOption "enables virtualbox";
   };
 
   config = lib.mkIf config.module.program.podman.enable {
-    virtualisation.podman = {
-      enable = true;
+    virtualisation = {
+      containers.enable = true;
+      containers.storage.settings = {
+        storage = {
+          driver = "overlay";
+          runroot = "/run/containers/storage";
+          graphroot = "/var/lib/containers/storage";
+          rootless_storage_path = "/tmp/containers-$USER";
+          options.overlay.mountopt = "nodev,metacopy=on";
+        };
+      };
 
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      oci-containers.backend = "podman";
+      podman = {
+        enable = true;
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+      };
     };
+    environment.systemPackages = with pkgs; [ docker-compose ];
+    environment.extraInit = ''
+      if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
+        export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+      fi
+    '';
   };
 }
